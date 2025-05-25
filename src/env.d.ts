2c1631068d43de8096722cba4c4519e389f85b4c
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-05-25 15:19:48 +0300

    Add @astrojs/ts-plugin as a devDependency and update env.d.ts for TypeScript support

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,17 @@
 /// <reference types="astro/client" />
-/// <reference types="astro/astro-jsx" />
+/// <reference types="@astrojs/ts-plugin" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_SITE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'astro-slot': any;
+    [elementName: string]: any;
+  }
+}
